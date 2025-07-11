ba3dc5be79d139b016b798cadefb7521f1d34af6
mryshkov <3hotpinguin@gmail.com>
2025-07-11 22:30:14 +0200

    Add files via upload

--- caesarCypher.js
+++ caesarCypher.js
@@ -20,7 +20,12 @@
 
 
 function Caesar(){
-	let alphabet, offset, output = "";
+	let alphabet, offset, output = "", message, messageArr, mode;
+
+	do{
+		mode = prompt("choose mode(coder - c, decoder - d");
+	} while(mode === String);
+	
 	let ukrAlphabet = [
 			"а", "б", "в", "г", "ґ", "д", "е", "є",
 			"ж", "з", "и", "і", "ї", "й", "к", "л",
@@ -30,7 +35,15 @@ function Caesar(){
 			"є", "ж", "з", "и", "і", "ї", "й", "к", 
 			"л", "м", "н", "о", "п", "р", "с", "т", 
 			"у", "ф", "х", "ц", "ч", "ш", "щ", "ь", 
-			"ю",  "я",
+			"ю", "я", "А", "Б", "В", "Г", "Ґ", "Д", 
+			"Е", "Є", "Ж", "З", "И", "І", "Ї", "Й", 
+			"К", "Л", "М", "Н", "О", "П", "Р", "С", 
+			"Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", 
+			"Ь", "Ю", "Я", "А", "Б", "В", "Г", "Ґ", 
+			"Д", "Е", "Є", "Ж", "З", "И", "І", "Ї", 
+			"Й", "К", "Л", "М", "Н", "О", "П", "Р", 
+			"С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", 
+			"Щ", "Ь", "Ю", "Я",
 		];
 		let engAlphabet = [
 			"a", "b", "c", "d", "e", "f", "g", "h", 
@@ -39,7 +52,13 @@ function Caesar(){
 			"y", "z", "a", "b", "c", "d", "e", "f", 
 			"g", "h", "i", "j", "k", "l", "m", "n", 
 			"o", "p", "q", "r", "s", "t", "u", "v",
-			"y", "z", 
+			"y", "z", "A", "B", "C", "D", "E", "F", 
+			"G", "H", "I", "J", "K", "L", "M", "N", 
+			"O", "P", "Q", "R", "S", "T", "U", "V", 
+			"W", "X", "Y", "Z", "A", "B", "C", "D", 
+			"E", "F", "G", "H", "I", "J", "K", "L", 
+			"M", "N", "O", "P", "Q", "R", "S", "T", 
+			"U", "V", "W", "X", "Y", "Z",
 	];
 
 	do{
@@ -55,9 +74,14 @@ function Caesar(){
   			alert("specify the correct alphabet(ukr/eng)");
   		}
 	} while(!Array.isArray(alphabet));
-
-	let message = prompt("enter original message");
-	let messageArr = message.split("");
+	if (mode == "c"){
+		message = prompt("enter original message");
+	}
+	else{
+		message = prompt("enter coded message");
+	}
+	
+	messageArr = message.split("");
 
 		do{
 		offset = +prompt("specify the offset");
@@ -65,23 +89,49 @@ function Caesar(){
   			alert("offset should be less than 33")
   		}
 		} while(offset > 33);
-  		
-		messageArr.forEach((e) => {
-			for (let i = 0; i <= alphabet.length; i++){
-				if (e === " "){
-				output += " ";
-				break;
-				} else if(e === ","){
-					output += ",";
+  	
+		if (mode == "c"){
+			messageArr.forEach((e) => {
+  			for (let i = 0; i <= alphabet.length; i++){
+  				if (e === " "){
+					output += " ";
 					break;
-				}
+ 				} else if(e === ","){
+ 					output += ",";
+ 					break;
+ 				}
 
-				if (e === alphabet[i]){
-					output += alphabet[i+offset];
+ 				if (e === alphabet[i]){
+  					output += alphabet[i+offset];
+  					break;
+				}	
+  			}
+  		})
+		} else {
+			messageArr.forEach((e) => {
+  			for (let i = 0; i <= alphabet.length; i++){
+  				if (e === " "){
+					output += " ";
 					break;
-			}	
-			}
-		})
+ 				} else if(e === ","){
+ 					output += ",";
+ 					break;
+ 				}
+
+ 				if (e === alphabet[i]){
+ 					if (alphabet.length == engAlphabet.length){
+ 						output += alphabet[26+i-offset];
+ 						break;
+ 					} else {
+ 						output += alphabet[33+i-offset];
+	  					break;
+ 					}
+				}	
+  			}
+  		})
+		}
+
+  		
 		return alert(output);
 }
 
